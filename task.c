ee8b3890c5d365406f34f3ae1291fc7f35eaf793
Alina <1647499849@qq.com>
2021-08-07 14:23:14 +0800

    枚举常量

--- task.c
+++ task.c
@@ -1,11 +1,40 @@
-#include<stdio.h>
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//#define MAX 10
+//int main()
+//{
+//	int arr[MAX] = { 0 };
+	//printf("%d\n", MAX);
+//	return 0;
+//}
+
+
+//enum Sex
+//{
+	//MALE,
+//	FEMALE,
+	//SECRET
+//};
+//int main()
+//{
+	//enum Sex s = MALE;
+	//printf("%d\n",MALE);
+	//printf("%d\n",FEMALE);
+	//printf("%d\n", SECRET);
+//	return 0;
+//}
+
+#include <stdio.h>
+enum Color
+{
+	RED,
+	YELLOW,
+	BLUE
+};
 int main()
 {
-	int num1 = 0;
-	int num2 = 0;
-	scanf_s("%d%d", &num1, &num2);
-	int sum = 0;
-	sum = num1 + num2;
-	printf("sum=%d\n", sum);
+	printf("%d\n", RED);
+	printf("%d\n", YELLOW);
+	printf("%d\n", BLUE);
 	return 0;
 }
