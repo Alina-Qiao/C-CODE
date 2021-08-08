142bcd9e0b804eafe3b0caeb41dae8ab6b3100fd
Alina <1647499849@qq.com>
2021-08-08 10:58:49 +0800

    转义代码

---
+++ task3.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+	//char arr1[] = "abc";
+	//char arr2[] = { 'a','b','c'};
+	//printf("%d\n", strlen(arr1));
+	//printf("%d\n", strlen(arr2));
+	//return 0;
+//}
+//int main()
+//{
+	//printf("abc\n");
+	//return 0;
+//}
+int main()
+{
+	//printf("c:\\test");
+	//printf("%c\n", '\'\'');
+	//printf("%s\n", "\"\"");
+	//printf("%d\n", strlen("c:\\test\\32\\test.c"));
+	//printf("%c\n", '\132');
+	//printf("%c\n", '\x32');
+	return 0;
+}
+//int main()
+//{
+	//printf("(are you ok\?\?)\n");
+	//return 0;
+//}
