6bf03bfbc4273fbbe9f5102a88b58627d326660d
Alina <1647499849@qq.com>
2021-08-09 11:38:15 +0800

    循环+数组

---
+++ task5.c
@@ -0,0 +1,65 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+//int main()
+//{
+//	int input = 0;
+//	printf("加入比特\n");
+//	printf("你要好好学习吗?(1/0)>:");
+//	scanf("%d", &input);
+//	if(input == 1)
+//		printf("好offer\n");
+//	else
+//		printf("卖红薯\n");
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int line = 0;
+//	printf("加入比特\n");
+//	while (line < 20000)
+//	{
+//		printf("敲一行代码:%d\n", line);
+//		line++;
+//	}
+//	if (line >= 20000)
+//		printf("好offer");
+//	return 0;
+//}
+//int Add(int x, int y)
+//{
+//	int z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int num1 = 10;
+//	int num2 = 20;
+//	int a = 100;
+//	int b = 200;
+//	int sum = 0;
+//	sum = Add(num1, num2);
+//	sum = Add(a, b);
+//	printf("sum=%d\n", sum);
+//	return 0;
+//}
+int main()
+{
+	/*int a = 1;
+	int b = 2;
+	int c = 3;
+	int d = 4;*/
+	int arr[10]={ 1,2,3,4,5,6,7,8,9,10 };
+	int i = 0;
+	while (i < 10)
+	{
+		printf("%d ", arr[i]);
+		i++;
+	}
+
+	//printf("% d\n", arr[4]);
+	/*char ch[20];
+	float arr2[5];*/
+	return 0;
+}
