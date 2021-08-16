5aa75225aeeebadb5d529175a487be957c1dcf5a
Alina <1647499849@qq.com>
2021-08-16 12:04:28 +0800

    do while循环

---
+++ task13.c
@@ -0,0 +1,102 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1;i <= 10;i++)
+//	{
+//		if (i == 5)
+//			continue;
+//			//break;
+//		printf("%d", i);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 10)
+//	{
+//		if (i == 5)
+//	
+//			continue;
+//		printf("%d", i);
+//				i++;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0;i < 10;i++)
+//	{
+//		if (i == 5)
+//			printf("haha\n");
+//		printf("hehe");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int i = 0;
+//	for (i = 0;i < 10;i++)
+//	{
+//		printf("%d", arr[i]);
+//	}
+//	return 0;
+//
+//}
+//int main()
+//{
+//	for (;;)
+//		printf("hehe\n");
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (;i < 10;i++)
+//	{
+//		
+//		for (;j < 10;j++)
+//		{
+//			printf("hehe\n");
+//		}
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//    int x, y;
+//    for (x = 0, y = 0;x < 2 && y < 5;++x, y++)
+//    {
+//        printf("hehe\n");
+//    }
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	int k = 0;
+//	for (i = 0, k = 0;k = 0;i++, k++)
+//		printf("%d ", i);
+//		k++;
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 1;
+//	do
+//	{
+//		if (i == 5)
+//			continue;
+//		printf("%d\n", i);
+//		i++;
+//	} 
+//	while (i <= 10);
+//	return 0;
+//
+//}
