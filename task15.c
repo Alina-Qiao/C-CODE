dd31ba9faac1ee2241317d8a22ef3e71284ae17d
Alina <1647499849@qq.com>
2021-08-18 18:17:12 +0800

    练习

--- task15.c
+++ task15.c
@@ -22,30 +22,55 @@
 //		printf("三次密码均错误，退出程序\n");
 //	return 0;
 //}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf("%d%d%d", &a, &b, &c);
+//		if (a < b)
+//		{
+//			int tmp = a;
+//			a = b;
+//			b = tmp;
+//		}
+//	if (a < c)
+//	{
+//		int tmp = a;
+//		a = c;
+//		c = tmp;
+//	}
+//	if (b < c)
+//	{
+//		int tmp = b;
+//		b = c;
+//		c = tmp;
+//	}
+//	printf("%d %d %d\n", a, b, c);
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1;i <= 100;i++)
+//	{
+//		if (i % 3 == 0)
+//			printf("%d ", i);
+//	}
+//	return 0;
+//
 int main()
 {
-	int a = 0;
-	int b = 0;
-	int c = 0;
-	scanf("%d%d%d", &a, &b, &c);
-		if (a < b)
-		{
-			int tmp = a;
-			a = b;
-			b = tmp;
-		}
-	if (a < c)
+	int m = 24;
+	int n = 18;
+	int r = 0;
+	scanf("%d%d", &m, &n);
+	while (m % n)
 	{
-		int tmp = a;
-		a = c;
-		c = tmp;
+		r = m % n;
+		m = n;
+		n = r;
 	}
-	if (b < c)
-	{
-		int tmp = b;
-		b = c;
-		c = tmp;
-	}
-	printf("%d %d %d\n", a, b, c);
+	printf("%d\n", n);
 	return 0;
 }
