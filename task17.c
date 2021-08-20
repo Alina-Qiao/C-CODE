39a2d26c883e8ad9c8308569a9a28b7708531a76
Alina <1647499849@qq.com>
2021-08-20 22:44:55 +0800

    练习4

---
+++ task17.c
@@ -0,0 +1,136 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//#include<math.h>
+//int main()
+//{		 
+//
+//	int i = 0;
+//	int count = 0;
+//	for (i = 100;i <= 200;i++)
+//	{
+//		int j = 0;
+//		for (j = 2;j <= sqrt(i);j++)
+//
+//		{
+//			if (i % j == 0)
+//			{
+//				break;
+//			}
+//		}
+//		if (j > sqrt(i))
+//		{
+//			count++;
+//			printf("%d", i);
+//		}
+//	}
+//	return 0;
+// }
+//int main()
+//{
+//	int a = 0, b = 0;
+//	for (a = 1, b = 1; a <= 100; a++)
+//	{
+//		if (b >=20)
+//			break;
+//		if (b % 3 == 1)
+//		{
+//			b = b + 3;
+//			continue;
+//		}
+//		b = b - 5;
+//	}
+//	printf("%d\n", a);
+//	return 0;
+//
+//}
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 1;i <= 100;i++)
+//	{
+//		if (i % 10 == 9)
+//			count++;
+//	    if (i / 10 == 9)
+//			count++;
+//	}
+//	printf("count=%d\n", count);
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	double sum = 0.0;
+//	int flag = 1;
+//	for (i = 1;i <= 100;i++)
+//	{
+//		sum += flag*1.0 / i;
+//		flag = -flag;
+//	}
+//	printf("%f\n", sum);
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[] = { -1,2,3,4,5,6,7,8,9,-10 };
+//	int max = arr[0];
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	for (i = 0;i < sz;i++)
+//	{
+//		if (arr[i] > max)
+//		{
+//			max = arr[i];
+//		}
+//	}
+//	printf("max=%d\n", max);
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1;i <= 9;i++)
+//	{
+//		int j = 1;
+//		for (j = 1;j <= i;j++)
+//		{
+//			printf("%d*%d=%2d ", i, j, i * j);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+void menu()
+{
+	printf("**************************************\n");
+	printf("****** 1. play      0.exit     *******\n");
+	printf("**************************************\n");
+}
+void game()
+{
+	printf("²ÂÊý×Ö\n");
+}
+int main()
+{
+	int input = 0;
+	do
+	{
+		menu();
+			printf("ÇëÑ¡Ôñ>:");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("ÍË³öÓÎÏ·\n");
+			break;
+		default:
+			printf("Ñ¡Ôñ´íÎó\n");
+				break;
+		}
+	} while (input);
+	return 0;
+}
