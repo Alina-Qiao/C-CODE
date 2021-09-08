adb5330c79f3414454263dff1addf29bc8625e62
Alina <1647499849@qq.com>
2021-09-08 11:40:22 +0800

    结构体传参

---
+++ task28.c
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//typedef struct S
+//{
+//	int a;
+//	char c;
+//	char arr[20];
+//	double d;
+//};
+//struct T
+//{
+//	char ch[10];
+//	struct S s;
+//	char* pc;
+//};
+//int main()
+//{
+//	char arr[] = "hello bit\n";
+//	struct T t = { "hehe",{100,'w',"hello word",3.14},arr};
+//	printf("%s\n", t.ch);
+//	printf("%s\n", t.s.arr);
+//	printf("%lf\n", t.s.d);
+//	printf("%s\n", t.pc);
+//	return 0;
+//}
+typedef struct Stu
+{
+	char name[20];
+	short age;
+	char tele[12];
+	char sex[5];
+}Stu;
+void Print1(Stu tmp)
+{
+	printf("name:%s\n", tmp.name);
+	printf("age:%d\n", tmp.age);
+	printf("tele:%s\n", tmp.tele);
+	printf("sex:%s\n", tmp.sex);
+}
+void Print2(Stu* ps)
+{
+	printf("name:%s\n", ps->name);
+	printf("age:%d\n", ps->age);
+	printf("tele:%s\n", ps->tele);
+	printf("sex:%s\n", ps->sex);
+}
+int main()
+{
+	Stu s = { "ÀîËÄ",40,"2222222","ÄĞ" };
+	Print1(s);
+	Print2(&s);
+	return 0;
+}
