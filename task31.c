909db50f03ce2dda8c1890375f1e0ad9e81d4846
Alina <1647499849@qq.com>
2021-09-12 15:46:29 +0800

    指针详解2

---
+++ task31.c
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//int main()
+//{
+//int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//int* p = arr;
+//int i = 0;
+//for (i = 0;i < 10;i++)
+//{
+//	printf("%d ", * (p + i));
+//}
+//	//int(*pa)[10] = &arr;
+//	//int i = 0;
+//	//fora(i = 0;i < 10;i++)
+//	//{
+//	//	printf("%d\n", *(*pa + i);
+//	//	//printf("%d",(*pa)[i);
+//	return 0;
+//}
+//void print1(int arr[3][5], int x, int y)
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0;i < x;i++)
+//	{
+//		for (j = 0;j < y;j++)
+//		{
+//			printf("%d "arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//}
+//void print2(int(*p)[5], int x, int y)
+//{
+//	int i = 0;
+//	for (i = 0;i < x;i++)
+//	{
+//		int j = 0;
+//		for (j = 0;j < y;j++)
+//		
+//			{
+//				printf("%d ", *(*(p + i) + j));
+//			}
+//		printf("\n");
+//		
+//
+//	}
+//	
+//}
+//int main()
+//{
+//	int arr[3][5] = { {1,2,3,4,5},{2,3,4,5,6},{3,4,5,6,7} };
+//	print1(arr, 3, 5);
+//	print2(arr, 3, 5);
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int i = 0;
+//	int* p = arr;
+//	for (i = 0;i < 10;i++)
+//	{
+//		printf("%d ", *(p + i));      
+//		printf("%d " *(arr + i));
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
