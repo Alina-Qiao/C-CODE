6f570a9ea84d4c9a4ad111373087c8cecba429fb
Alina <1647499849@qq.com>
2021-11-09 21:02:41 +0800

    程序设计

--- test5.cpp
+++ test5.cpp
@@ -3,17 +3,35 @@
 using namespace std;
 int main()
 {
-	int x;
-    cout<<"请输入一个数"<<endl;
-	cin>>x;
-	if(x%2==0)
+//	int x;
+//    cout<<"请输入一个数"<<endl;
+//	cin>>x;
+//	if(x%2==0)
+//	{
+//		cout<<"yes"<<endl;
+//		
+//	}
+//	else
+//	cout<<"No"<<endl;
+//	
+int a;
+cout<<"请输入你的成绩"<<endl; 
+cin>>a;
+	if(a>=90)
 	{
-		cout<<"yes"<<endl;
-		
+		cout<<"优秀"<<endl; 
+	}
+	else if(a>=80&&a<=90)
+	{
+		cout<<"良好"<<endl; 
+	}
+	else if(a>=70&&a<=80)
+	{
+		cout<<"中等"<<endl;
 	}
 	else
-	cout<<"No"<<endl;
-	
-	
+	{
+		cout<<"不及格"<<endl; 
+	}
 	return 0;
 }
