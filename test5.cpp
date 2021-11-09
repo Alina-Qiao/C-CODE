ba3ccac76a17991e1e8b1b2d25091cd06d1a5933
Alina <1647499849@qq.com>
2021-11-09 17:04:30 +0800

    程序设计

---
+++ test5.cpp
@@ -0,0 +1,19 @@
+#include<iostream> 
+#include<iomanip>
+using namespace std;
+int main()
+{
+	int x;
+    cout<<"请输入一个数"<<endl;
+	cin>>x;
+	if(x%2==0)
+	{
+		cout<<"yes"<<endl;
+		
+	}
+	else
+	cout<<"No"<<endl;
+	
+	
+	return 0;
+}
