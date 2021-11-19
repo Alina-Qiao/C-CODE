743a5a041a018c31f93917ae7a7f2ec0ca831d74
Alina <1647499849@qq.com>
2021-11-19 23:59:45 +0800

    数组

---
+++ test9.cpp
@@ -0,0 +1,47 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+// int A[5]={1,2,3,4,5};
+// int i;
+// int B[5];
+// for(i=0;i<5;i++)
+//    B[i]=A[i];
+//    cout<<"B的数组为：";
+// for(i=0;i<5;i++)
+//   cout<<B[i]<<' ';
+//  return 0;
+//}//将一个一维数组的元素赋给另一个等长的一维数组 
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int A[3][4],i,j;
+//	cout<<"请输入12个整数: ";
+//	for(i=0;i<3;i++)
+//	  for(j=0;j<4;j++)
+//	  cin>>A[i][j];
+//	  cout<<"输出为：\n";
+//	  for(i=0;i<3;i++)
+//	  {
+//	    for(j=0;j<4;j++)
+//	       cout<<A[i][j]<<" ";
+//	  cout<<endl;
+//	  }
+//	return 0;
+//}
+//输入5个整数并将5个整数反向输出 
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int a[5];
+//	int i;
+//	cout<<"请输入5个整数"<<endl; 
+//	for(i=0;i<5;i++)
+//	cin>>a[i];
+//	cout<<"反向输出的5个数为："; 
+//	 for(i=4;i>=0;i--)
+//	 cout<<a[i]<<' ';
+//	return 0;
+//}
