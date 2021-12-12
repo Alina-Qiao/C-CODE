1fad06834f68063a6ca7ab0fdf1a47ba0b881d2d
Alina <1647499849@qq.com>
2021-12-12 14:00:15 +0800

    12道经典题

---
+++ test1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"陆小桥"<<"数媒"<<"20210805117"<<endl;
+	int a,b,c;
+	cout<<"输入两个数："<<endl;
+	cin>>a>>b;
+	c=a*b;
+	cout<<"a*b= "<<c<<endl;
+	return 0;
+} 
