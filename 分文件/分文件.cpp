3b2cc5255ad70710c830527302838160f1f2d399
Alina <1647499849@qq.com>
2021-12-31 21:04:54 +0800

    分文件

---
+++ 分文件/分文件.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include"Add.h"
+using namespace std;
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int c=Add(a, b);
+	cout << c << endl;
+	return 0;
+}
